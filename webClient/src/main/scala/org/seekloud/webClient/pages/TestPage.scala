abe07196cb942e011320dbf24415aa4f7f4e6950
xusiran@neotel.cpm.cn <Xsr979708>
2020-03-01 20:35:22 +0800

    file

--- webClient/src/main/scala/org/seekloud/webClient/pages/TestPage.scala
+++ webClient/src/main/scala/org/seekloud/webClient/pages/TestPage.scala
@@ -71,7 +71,7 @@ class TestPage(id: String) extends Page{
         <button onclick={(e: Event)=>wsClient.sendByteMsg(MediaControlReq(10011, 10012))}>房间10011控制10012</button>
         <button onclick={(e: Event)=>wsClient.sendByteMsg(SpeakReq(10011, id.toLong))}>申请说话</button>
         <button onclick={(e: Event)=>wsClient.sendByteMsg(SpeakRsp(10011, 10012, true))}>10012同意说话</button>
-        <button onclick={(e: Event)=>wsClient.sendByteMsg(KickOutReq(10011, 10012))}>10011踢出10012</button>
+        <button onclick={(e: Event)=>wsClient.sendByteMsg(KickOutReq(10011, 10001))}>10011踢出10001</button>
         <button onclick={(e: Event)=>wsClient.sendByteMsg(Disconnect)}>断线</button>
       </div>
       <div class="text-list">
